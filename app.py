e716ef9acdca024bab5e3efe1b2073329ed78702
VivianLu <vian0927@gmail.com>
2023-01-12 11:50:48 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    # return '<h1>Hello!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -18,6 +19,21 @@ def test_url_for():
     print(url_for('test_url_for', num=2))
     return 'Test Page'
 
+
+
+name = 'Vivian Lu'
+movies = [
+{'title': 'Fantastic Beasts and Where to Find Them', 'year': '2016'},
+{'title': 'Crazy Rich Asians', 'year': '2018'},
+{'title': 'Captain Marvel', 'year': '2019'},
+{'title': 'Gemini Man', 'year': '2020'},
+{'title': 'Spider-Man: No Way Home', 'year': '2021'},
+{'title': 'Shang-Chi and the Legend of the Ten Rings', 'year': '2021'},
+{'title': 'Venom: Let There Be Carnage', 'year': '2021'},
+{'title': 'Eternals', 'year': '2021'},
+{'title': 'Avatar: The Way of Water', 'year': '2022'},
+{'title': 'Doctor Strange in the Multiverse of Madness', 'year': '2022'},
+]
 
 if __name__ == '__main__':
     app.run()
